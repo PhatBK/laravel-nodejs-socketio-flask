5d5396002f1cf52d972970e9975a5dd17df318ab
PhatBK <phatsn11081996@gmail.com>
2019-04-23 21:11:17 +0700

    update big-project v1 2019-04-23

--- app/Http/Controllers/RecommendController.php
+++ app/Http/Controllers/RecommendController.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use Goutte\Client;
+//
+//use Illuminate\Http\Request;
+//use Goutte\Client;
 use GuzzleHttp\Client as GuzzleClient;
-use GuzzleHttp\Exception\RequestException;
-use GuzzleHttp\Psr7\Response;
+
+//use GuzzleHttp\Exception\RequestException;
+//use GuzzleHttp\Psr7\Response;
 
 
 class RecommendController extends Controller
@@ -14,25 +15,19 @@ class RecommendController extends Controller
     public $data_set = "";
 	
     public function get_api(){
-  
     	$client = new GuzzleClient(['base_uri' => 'http://127.0.0.1:5000/']);
     	$res = $client->request('GET', 'api_get/');
-
     	$code = $res->getStatusCode(); // 200
 		$reason = $res->getReasonPhrase(); // OK
-		
     	dd($res);
     }
     public function post_api(){
-    	
     	$client = new GuzzleClient(['base_uri' => 'http://127.0.0.1:5000/']);
     	$res = $client->request('POST','api_post/');
     	$body = $res->getBody();
     	dd((string)$body);
-
     }
     public function get_api_data_user(){
-    	
     	$client = new GuzzleClient(['base_uri' => 'http://127.0.0.1:5000/']);
     	$res = $client->request('GET','api_get_data/user');
     	$body = $res->getBody();
@@ -42,7 +37,6 @@ class RecommendController extends Controller
             echo $us[1];
             echo "<br>";
         }
-        
     }
     public function get_api_data_monan(){
     	$client = new GuzzleClient(['base_uri' => 'http://127.0.0.1:5000/']);
@@ -66,6 +60,11 @@ class RecommendController extends Controller
 
     }
     public function rollBackAll() {
+        return response()->json(null);
+    }
+
+    public function shareData()
+    {
         return response()->json(null);
     }
 }
